e1a693870d25fb0f30c035f6bf1680617e804253
Isak Forssén <isak.forssen@hitachigymnasiet.se>
2025-02-05 12:33:36 +0100

    updated admin page

--- quasar-project/src/pages/AdminPage.vue
+++ quasar-project/src/pages/AdminPage.vue
@@ -1,9 +1,19 @@
 <template>
+    <body>
     <div>
         <h1>Admin Page</h1>
-        <GraphGeneric :data="chartData" />
+        <span class="row">
+            
+                <GraphGeneric :data="chartData" />
+          
+            
+           
+                <div class="idk"></div>
+            
+        </span>
         <GreenHouseController />
     </div>
+    </body> 
   </template>
 
 <script lang='ts'>
@@ -40,5 +50,48 @@ export default {
 
 
 <style scoped>
+h1 { 
+    color: rgb(77, 39, 11);
+    background-color: white;
+    margin: 20px 50px 10px 50px;
+    padding: 10px 25px;
+    border: 10px;
+    border-radius: 20px;
+    border-color: white;
+    border-style: solid;
+    font-size: 6em;
+    font-family: "Times New Roman", Times, serif;
+    font-weight: bold; 
+    font-stretch: expanded;
+    text-align: center;
+}
+
+body {
+    background-image: url("../assets/greenhouse-background.jpg");
+    max-width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.row {
+    flex-direction: row;
+}
+
+.idk {
+    background-image: url("../assets/dark-greenery.jpeg");
+    max-width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    border: 7px solid white;
+    width: 250px;
+    height: 250px;
+    border-radius: 200px;
+    margin: 50px 70px;
+
+}
+
 
 </style>
